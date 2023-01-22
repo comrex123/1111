323d446ee4ec0da4a667e074c78c6f6e1a20fc3d
Idan <bacharidan1989@gmail.com>
2023-01-22 03:22:20 -0500

    Initial commit

--- cve_check.rb
+++ cve_check.rb
@@ -14,7 +14,7 @@ g.add
 # Commit the added files
 g.commit("Initial commit")
 
-remote_url = 'https://comrex123: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@github.com/comrex123/new_123repo.git'
+remote_url = 'https://comrex123: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@github.com/comrex123/1111.git'
 
 # Add the remote repository to your local repository
 g.add_remote('1234origin12341', remote_url)
